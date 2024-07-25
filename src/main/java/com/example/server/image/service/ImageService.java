204e925f3bfa058bebff01fbf3c7c019167d268d
matt <matt1235@naver.com>
2024-07-25 15:22:09 +0900

    feat: 이미지 업로드 기능 구현 (#16)

--- src/main/java/com/example/server/image/service/ImageService.java
+++ src/main/java/com/example/server/image/service/ImageService.java
@@ -1,15 +1,18 @@
 package com.example.server.image.service;
 
+import com.example.server.feed.repository.FeedJpaRepository;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.geometry.Positions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
@@ -23,23 +26,39 @@ public class ImageService {
     @Value("${aws.s3.bucket}")
     private String bucketName;
 
-    private final Logger logger = LoggerFactory.getLogger(ImageService.class);
+    private final FeedJpaRepository feedJpaRepository;
 
     public String saveFile(MultipartFile file) throws IOException {
         String imageId = UUID.randomUUID().toString();
-        String fileName = "feed/" + imageId + ".jpg";
+        BufferedImage original = ImageIO.read(file.getInputStream());
 
-        try (InputStream inputStream = file.getInputStream()) {
+        File imageFile = File.createTempFile(imageId, ".jpg");
+        File thumbnailFile = File.createTempFile(imageId, ".jpg");
+
+        try {
+            Thumbnails.of(original).scale(1.0d).outputFormat("jpg").toFile(imageFile);
+            Thumbnails.of(original).crop(Positions.CENTER).size(500, 500).outputFormat("jpg").toFile(thumbnailFile);
+
+            uploadToS3(imageFile, "feed/" + imageId + ".jpg");
+            uploadToS3(thumbnailFile, "thumbnail/" + imageId + ".jpg");
+        } finally {
+            imageFile.delete();
+            thumbnailFile.delete();
+        }
+
+        return imageId;
+    }
+
+    private void uploadToS3(File file, String key) throws IOException {
+        try (InputStream inputStream = file.toURI().toURL().openStream()) {
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                     .bucket(bucketName)
-                    .key(fileName)
-                    .contentType("image/jpeg") // Content-Type 설정
+                    .key(key)
+                    .contentType("image/jpeg")
                     .build();
 
-            PutObjectResponse response = s3Client.putObject(putObjectRequest,
-                    software.amazon.awssdk.core.sync.RequestBody.fromInputStream(inputStream, file.getSize()));
-
-            return imageId;
+            s3Client.putObject(putObjectRequest,
+                    software.amazon.awssdk.core.sync.RequestBody.fromInputStream(inputStream, file.length()));
         }
     }
 }
